bced982c616c369bfb734eec3fcc54774aedd981
maro1 <maro1@alu.ua.es>
2024-02-26 16:25:23 +0100

    49372123R creados constructores de Coordenada.cs

--- hada-p2/Coordenada.cs
+++ hada-p2/Coordenada.cs
@@ -4,9 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace hada_p2
+namespace Hada
 {
     class Coordenada
     {
+
+        private int _Fila;
+        
+        public int Fila
+        {
+            get { return _Fila; }
+
+        }
+
+        private int _Columna;
+
+        public int Columna
+        {
+            get { return _Columna; }
+
+        }
+
+        public Coordenada()
+        {
+
+        }
+
+        public Coordenada(int fila,int columna)
+        {
+            _Fila = fila;
+            _Columna = columna;
+        }
+
+        public Coordenada(string fila,string columna)
+        {
+
+        }
+
+        public Coordenada(Coordenada coord)
+        {
+
+        }
     }
 }
