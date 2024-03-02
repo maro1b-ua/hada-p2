4b6780d4aa03f15533d2b239920740baba46d171
Carlos <cfcl1@gcloud.ua.es>
2024-03-02 22:20:35 +0100

    50383440P He finalizado el constructor de la clase barco.cs

--- hada-p2/Barco.cs
+++ hada-p2/Barco.cs
@@ -8,7 +8,7 @@ namespace Hada
 {
     class Barco
     {
-        private readonly Dictionary<Coordenada, String> _CoordenadasBarco ;
+        private readonly Dictionary<Coordenada, String> _CoordenadasBarco = new Dictionary<Coordenada, string>();
 
         public Dictionary<Coordenada, String> CoordenadasBarco => _CoordenadasBarco;
 
@@ -20,7 +20,36 @@ namespace Hada
 
         public int NumDanyos => _NumDanyos;
 
+        public Barco(string nombre, int longitud, char orientacion, Coordenada coordenadaInicio)
+        {
+            int fila =coordenadaInicio.Fila;
+            int columna = coordenadaInicio.Columna;
 
+            this._Nombre = nombre;
+            this._CoordenadasBarco.Add(coordenadaInicio, nombre);
+            this._NumDanyos = 0;
+
+            if(orientacion=='v')
+            {
+                for(int i=0; i<longitud; i++)
+                {
+                    fila++;
+                    Coordenada coordenadaSiguiente = new Coordenada(fila, columna);
+                    this.CoordenadasBarco.Add(coordenadaSiguiente, nombre);
+                }
+            }
+            else if (orientacion == 'h')
+            {
+                for (int i = 0; i < longitud; i++)
+                {
+                    columna++;
+                    Coordenada coordenadaSiguiente = new Coordenada(fila, columna);
+                    this.CoordenadasBarco.Add(coordenadaSiguiente, nombre);
+                }
+            }
+        }
+
+       
 
     }
 }
