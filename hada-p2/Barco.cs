514bfdc4bd1cbb456af9f0ade85cd10feb45760b
Carlos <cfcl1@gcloud.ua.es>
2024-03-02 20:47:56 +0100

    50383440P creados las propiedades publicas CoordenadasBarco, Nombre y NumDanyos de la clase barco.cs

--- hada-p2/Barco.cs
+++ hada-p2/Barco.cs
@@ -8,5 +8,19 @@ namespace Hada
 {
     class Barco
     {
+        private readonly Dictionary<Coordenada, String> _CoordenadasBarco ;
+
+        public Dictionary<Coordenada, String> CoordenadasBarco => _CoordenadasBarco;
+
+        private string _Nombre;
+
+        public string Nombre => _Nombre;
+
+        private int _NumDanyos;
+
+        public int NumDanyos => _NumDanyos;
+
+
+
     }
 }
