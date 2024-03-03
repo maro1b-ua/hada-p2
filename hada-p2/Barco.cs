1d6aef82095e0107f2cbb3fd41bc8e4952856472
Carlos <cfcl1@gcloud.ua.es>
2024-03-03 01:36:50 +0100

    50383440P Se han finalizado los metodos disparar() y hundido() de la clase barco.cs

--- hada-p2/Barco.cs
+++ hada-p2/Barco.cs
@@ -8,18 +8,26 @@ namespace Hada
 {
     class Barco
     {
+
+        //Diccionario donde se guardan las coordenadas que ocupa el barco siendo la clave la coordenada y el valor el nombre del barco
+
         private readonly Dictionary<Coordenada, String> _CoordenadasBarco = new Dictionary<Coordenada, string>();
 
         public Dictionary<Coordenada, String> CoordenadasBarco => _CoordenadasBarco;
+
+        //Nombre del barco
 
         private string _Nombre;
 
         public string Nombre => _Nombre;
 
+        //Daño recibido por el barco
+
         private int _NumDanyos;
 
         public int NumDanyos => _NumDanyos;
 
+        //constructor de la clase
         public Barco(string nombre, int longitud, char orientacion, Coordenada coordenadaInicio)
         {
             int fila =coordenadaInicio.Fila;
@@ -48,8 +56,42 @@ namespace Hada
                 }
             }
         }
+        //Metodo de la clase en el que el barco realiza un disparo en una coordenada
+        public void Disparo(Coordenada c)
+        {
+            Dictionary<Coordenada, string>.KeyCollection coordenadaDisparo = this.CoordenadasBarco.Keys;
+            foreach (Coordenada coord in coordenadaDisparo)
+            {
+                if (c == coord)
+                {
+                    string situacion = _CoordenadasBarco[coord];
+                    this._CoordenadasBarco[coord] = situacion + "_T";
+                    int danyos = NumDanyos;
+                    danyos++;
+                    eventoTocado(this, new TocadoArgs(_Nombre, c, _CoordenadasBarco[coord]));
+                    if (hundido() == true)
+                    {
+                        eventoHundido(this, new HundidoArgs(_Nombre));
+                    }
+                }
+            }
+        }
 
-       
+        public bool hundido()
+        {
+            Dictionary<Coordenada, string>.KeyCollection coordenadaHundido = this.CoordenadasBarco.Keys;
+            foreach(Coordenada coord in coordenadaHundido)
+            {
+                string situacion = _CoordenadasBarco[coord];
+                if(situacion == _Nombre)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
 
+        public event EventHandler<TocadoArgs> eventoTocado;
+        public event EventHandler<HundidoArgs> eventoHundido;
     }
 }
