619ee9ffa4c55def344b814c2da0d4d87330f322
maro1 <maro1@alu.ua.es>
2024-03-04 16:49:22 +0100

    49372123 Clase Game empezada

--- hada-p2/Game.cs
+++ hada-p2/Game.cs
@@ -8,5 +8,29 @@ namespace Hada
 {
     class Game
     {
+
+        private bool finPartida;
+        private Tablero tablero;
+
+        private void GameLoop()
+        {
+            finPartida = false;
+            tablero = new Tablero;
+
+            InicializaBarcos();
+            InicializaTablero();
+
+            GameLoop();
+        }
+
+        private void InicializaBarcos()
+        {
+
+        }
+
+        private void InicializaTablero()
+        {
+
+        }
     }
 }
