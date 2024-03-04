f225e680cd6a12e2cf83c214c1adf7129030e8ee
maro1 <maro1@alu.ua.es>
2024-03-04 16:33:50 +0100

    49372123R Implementada Coordenada.cs

--- hada-p2/Coordenada.cs
+++ hada-p2/Coordenada.cs
@@ -14,6 +14,14 @@ namespace Hada
         public int Fila
         {
             get { return _Fila; }
+            set
+            {
+                if (Fila < 0)
+                    throw new ArgumentOutOfRangeException(nameof(Fila),
+                          "El valor de la fila debe ser positivo");
+
+                _Fila = Fila;
+            }
 
         }
 
@@ -22,11 +30,21 @@ namespace Hada
         public int Columna
         {
             get { return _Columna; }
+            set
+            {
+                if (Columna < 0)
+                    throw new ArgumentOutOfRangeException(nameof(Columna),
+                          "El valor de la columna debe ser positivo");
+
+                _Columna = Columna;
+            }
 
         }
 
         public Coordenada()
         {
+            _Fila = 0;
+            _Columna = 0;
 
         }
 
@@ -38,12 +56,51 @@ namespace Hada
 
         public Coordenada(string fila,string columna)
         {
+            _Fila = int.Parse(fila);
+            _Columna = int.Parse(columna);
 
         }
 
         public Coordenada(Coordenada coord)
         {
-
+            _Fila = coord.Fila;
+            _Columna = coord.Columna;
         }
+
+        override
+        public string ToString()
+        {
+            return $"({_Fila},{_Columna})";
+        }
+
+        override
+        public int GetHashCode()
+        {
+            return this.Fila.GetHashCode() ^ this.Columna.GetHashCode();
+        }
+
+        override
+        public bool Equals(Object o)
+        {
+            if(o == null )
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool Equals(Coordenada coord)
+        {
+            if (_Fila == coord.Fila && _Columna == coord.Columna)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
     }
 }
